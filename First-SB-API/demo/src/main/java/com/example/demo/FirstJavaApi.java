2f836eee74de0cb17c41cc04434efc6c33f1ec9c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-27 06:21:41 -0500

    created first get route, just a simple hello world like route that returns text when a get request is sent to the /hello route

--- First-SB-API/demo/src/main/java/com/example/demo/FirstJavaApi.java
+++ First-SB-API/demo/src/main/java/com/example/demo/FirstJavaApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstspringapiApplication {
+public class FirstJavaApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstspringapiApplication.class, args);
+		SpringApplication.run(FirstJavaApi.class, args);
 	}
 
 }
