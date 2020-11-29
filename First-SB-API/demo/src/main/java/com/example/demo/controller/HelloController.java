28c1f8d3b7ac90384cde3f3212aac0198cea67a6
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-29 18:06:31 -0500

    update controler for testing

--- First-SB-API/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ First-SB-API/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,10 @@ public class HelloController {
     @RequestMapping("/hello")
     public String getHelloMessage(){
         return "Hello there user!";
+    }
+
+    @GetMapping("/")
+    public User getUser() {
+
     }
 }
