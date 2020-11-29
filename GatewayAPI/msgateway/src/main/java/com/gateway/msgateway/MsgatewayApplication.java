3ea1685a64e99bc2da9b0f0038cfd375e14b3414
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-28 23:23:06 -0500

    update gateway

--- GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/MsgatewayApplication.java
+++ GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/MsgatewayApplication.java
@@ -1,5 +1,7 @@
 package com.gateway.msgateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MsgatewayApplication {
