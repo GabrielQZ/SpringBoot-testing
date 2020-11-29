cc1a546e1ab868a1fe050e3432ead06ea8413fe1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-29 10:08:49 -0500

    update gateway router with testing routes, learning about encrypting the enviorment vars

--- GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/GatewayRouter.java
+++ GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/GatewayRouter.java
@@ -1,20 +1,43 @@
 package com.gateway.msgateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class GatewayRouter {
+
+    @Autowired
+    private Environment env;
+
+    //TESTING ROUTES
     @RequestMapping("/testmsg")
     public String getHelloMessage(){
         return "Hello there user!";
+    }
+
+    @GetMapping("/testenvprop")
+    public String getProps(){
+        return env.getProperty("authSystem.endpoint");
     }
 
     @PutMapping("/update")
     public String putTestUpdate(){
         return "updating";
     }
+
+    //MAIN ROUTES
+    //this gateway acts as the one entry point the the entire micro-service system. Some routes are public and some are authorized.
+    //authorized routes will pass along headers (where auth keys will be stored) and public routes will not
+    //endpoints to other MS servers will be stored privately in the gateways code
+
+    //AUTHORIZED ROUTE
+
+    //PUBLIC ROUTE
 
 }
 
--- GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/MsgatewayApplication.java
+++ GatewayAPI/msgateway/src/main/java/com/gateway/msgateway/MsgatewayApplication.java
@@ -1,9 +1,11 @@
 package com.gateway.msgateway;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class MsgatewayApplication {
 
 	public static void main(String[] args) {
