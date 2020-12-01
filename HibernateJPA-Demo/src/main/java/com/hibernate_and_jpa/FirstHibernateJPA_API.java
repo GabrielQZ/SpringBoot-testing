6144ee42f1d506c82f62739be31aa4426ff8579b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-30 23:04:19 -0500

    create main app file

---
+++ HibernateJPA-Demo/src/main/java/com/hibernate_and_jpa/FirstHibernateJPA_API.java
@@ -0,0 +1,13 @@
+package com.hibernate_and_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHibernateJPA_API {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstHibernateJPA_API.class, args);
+    }
+
+}
