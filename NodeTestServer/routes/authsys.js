3f34f01eb3ba3f7ae936e9240fc8c939477802a7
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-01 16:46:36 -0500

    update auth test routes to fully test get/delete actions and further test put request actions

--- NodeTestServer/routes/authsys.js
+++ NodeTestServer/routes/authsys.js
@@ -2,11 +2,25 @@ const router = require('express').Router();
 
 router.post('/', (req, res) => {
 
-  console.log(req.body.routing());
-
   res.json({
     message: "user created"
   })
 })
 
+router.put("/login", (req, res) => {
+  res.json(req.body);
+})
+
+router.get('/id/:id', (req, res) => {
+  res.json({
+    user: "ID:" + req.params.id
+  })
+})
+
+
+router.delete('/id/:id', (req, res) => {
+  res.json({
+    user: "ID:" + req.params.id
+  })
+})
 module.exports = router;
