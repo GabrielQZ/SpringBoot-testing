4c098129651386713f572a7663b4ddd5840102ae
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-30 23:05:55 -0500

    hide env vars/ application properties, update tests

---
+++ HibernateJPA-Demo/src/test/java/com/hibernate_and_jpa/FirstHibernateJPA_API_test.java
@@ -0,0 +1,13 @@
+package com.hibernate_and_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHibernateJPA_API_test {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
