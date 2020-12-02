5d6c2bf542e990125612c606f7d3358ea15dfdcb
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-01 22:18:17 -0500

    initialize user authentification service

---
+++ user-system/src/main/java/com/msauth/usersystem/UserSystemAuthApplication.java
@@ -0,0 +1,13 @@
+package com.msauth.usersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSystemAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSystemAuthApplication.class, args);
+	}
+
+}
---
+++ user-system/src/test/java/com/msauth/usersystem/UserSystemAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msauth.usersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
