399e46405d77c3c18424daaa147f3f969b42472e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-09 01:25:44 -0500

    update model to extract user properties from request data using anotations and  simple mapping function

--- AuthServiceAPI/src/main/java/com/microserve/authService/model/User.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/model/User.java
@@ -1,8 +1,12 @@
 package com.microserve.authService.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.json.JSONObject;
 
 import javax.persistence.*;
+import java.beans.Transient;
+import java.util.Map;
 import java.util.UUID;
 
 @Entity
@@ -12,17 +16,20 @@ public class User {
     @Id
     @GeneratedValue
     public UUID id;
-    @Transient
-    public Object user;
     @Column
     public String email;
     @Column
     public String password;
 
+    @JsonProperty("user")
+    public void unpackData(Map<String, String> user) {
+        email = user.get("email");
+        password = user.get("password");
+    }
+
     public StrippedUser strip() {
         return new StrippedUser(this);
     }
-
 
 }
 
