f97725e1831958685b33c202ecdf849cc478fea4
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-08 23:39:32 -0500

    update User repo to use UUID as the unique key for a user

--- AuthServiceAPI/src/main/java/com/microserve/authService/repository/UserRepository.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.microserve.authService.repository;
 import com.microserve.authService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 
 
 }
