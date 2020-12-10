2508876ca841d102786aae2e1d1b4c9224648b41
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-09 22:35:04 -0500

    create methods to check for repeated email/username

--- AuthServiceAPI/src/main/java/com/microserve/authService/repository/UserRepository.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.microserve.authService.repository;
 import com.microserve.authService.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    int countByName(String name);
+
+    int countByEmail(String email);
 
 }
