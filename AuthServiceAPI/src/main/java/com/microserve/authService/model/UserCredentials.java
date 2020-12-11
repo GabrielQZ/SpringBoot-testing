3cccc8f3e2b933850af98bf56eeed3545030e2bc
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-11 18:11:18 -0500

    update app to not use spring security default config, update usercredential json mapping to make more sense

--- AuthServiceAPI/src/main/java/com/microserve/authService/model/UserCredentials.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/model/UserCredentials.java
@@ -1,12 +1,22 @@
 package com.microserve.authService.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+
 public class UserCredentials {
     public String credential;
     public String password;
 
-    public UserCredentials( String credential, String password ) {
-        this.credential = credential;
-        this.password = password;
+    @JsonProperty("credentials")
+    public void unpackData(Map<String, String> user) {
+        credential = user.get("credential");
+        password = user.get("password");
+    }
+
+    public void sanitizeData () {
+        credential = credential != null ? credential.strip() : null;
+        password = password != null ? password.strip() : null;
+
     }
 
 }
