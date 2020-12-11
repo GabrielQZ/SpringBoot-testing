485e2c9ca3c9498147e313b814ef7c97a3c79d30
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-11 14:48:01 -0500

    class to create JWTs for users signing in

--- AuthServiceAPI/src/main/java/com/microserve/authService/validator/UserJWT.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/validator/UserJWT.java
@@ -1,2 +1,58 @@
-package com.microserve.authService.validator;public class UserJWT {
+package com.microserve.authService.validator;
+
+import com.microserve.authService.model.User;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class UserJWT {
+
+    @Autowired
+    Environment env;
+
+    public Object createJWT(User foundUser) {
+        try {
+            Instant now = Instant.now();
+
+            Date issuedAt = Date.from(now);
+            Date expiresAt = Date.from(now.plus(2, ChronoUnit.HOURS));
+
+            SecretKey key = Keys.hmacShaKeyFor(env.getProperty("jwt.key").getBytes());
+
+            String jwt = Jwts
+                    .builder()
+                    .setSubject("user-auth")
+                    .setIssuedAt(issuedAt)
+                    .setExpiration(expiresAt)
+                    .claim("username", foundUser.username)
+                    .claim("id", foundUser.id)
+                    .signWith(key)
+                    .compact();
+
+            JSONObject loginData = new JSONObject();
+            loginData.put("token", jwt);
+
+            return loginData;
+
+        } catch (JwtException e) {
+            System.out.println(e.getMessage());
+            return UserValidationErrors.jwtError(e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("\n\nJWT secret must not be set in env vars");
+            System.out.println(e.getMessage());
+
+            return UserValidationErrors.loggingInServerError();
+        } catch ( Exception e) {
+            System.out.println(e.getMessage());
+            return UserValidationErrors.loggingInServerError();
+        }
+    }
 }
