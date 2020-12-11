b7069c63bb5ef42671cb22e74ae8ba7ee15c1a11
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-12-11 14:45:17 -0500

    create a class to map to login request data

--- AuthServiceAPI/src/main/java/com/microserve/authService/model/UserCredentials.java
+++ AuthServiceAPI/src/main/java/com/microserve/authService/model/UserCredentials.java
@@ -1,2 +1,12 @@
-package com.microserve.authService.model;public class UserCredentials {
+package com.microserve.authService.model;
+
+public class UserCredentials {
+    public String credential;
+    public String password;
+
+    public UserCredentials( String credential, String password ) {
+        this.credential = credential;
+        this.password = password;
+    }
+
 }
